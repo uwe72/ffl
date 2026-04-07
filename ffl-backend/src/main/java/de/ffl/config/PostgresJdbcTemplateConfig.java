7659878515062f81c10193a6ac42fa4c69d5f076
Uwe Clement <uwe.clement@gmail.com>
2026-04-07 15:15:31 +0200

    Add @Primary PostgreSQL DataSource for JPA/Hibernate

--- ffl-backend/src/main/java/de/ffl/config/PostgresJdbcTemplateConfig.java
+++ ffl-backend/src/main/java/de/ffl/config/PostgresJdbcTemplateConfig.java
@@ -1,6 +1,8 @@
 package de.ffl.config;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -11,6 +13,13 @@ import javax.sql.DataSource;
 @Configuration
 @ConditionalOnProperty(name = "spring.profiles.active", havingValue = "docker")
 public class PostgresJdbcTemplateConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return new HikariDataSource();
+    }
 
     @Bean(name = "postgresJdbcTemplate")
     @Primary
